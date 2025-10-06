69ad8454e7a2616c4a8deee2839d1cf421c21c8e
Marco Rehberg <marcorehberg@gmx.net>
2025-10-06 09:42:53 +0200

    CSS kram

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,15 +1,24 @@
 const AboutPage = () => {
   return (
-    <div className="border p-5">
+    <div className="rounded-2xl p-5 py-15">
       <h2 className="text-4xl mb-4">Über diese Projekt</h2>
 
-      <h3 className="mb-4 text-xl">
+      <h3 className="mb-6 text-xl">
         1. Eingesetzt wurden folgende Komponenten und Techniken:
       </h3>
-      <p>
-        VITE - REACT - useEffect(), useState(), createContext(), use(),
-        react-router(), useActionState()
-      </p>
+      <ul class="grid grid-cols-2 gap-2 list-disc pl-5 text-2xl">
+        <li>VITE</li>
+        <li>REACT</li>
+        <li>useEffect()</li>
+        <li>useState()</li>
+        <li>createContext()</li>
+        <li>use()</li>
+        <li>react-router()</li>
+        <li>useActionState()</li>
+        <li>tailwindCss</li>
+        <li>DaisyUiCss</li>
+        <li>VanillaJS</li>
+      </ul>
     </div>
   );
 };
